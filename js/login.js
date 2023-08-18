bd68aa9712995477b9eed1efe59869f1eb3dd2bd
rodrigodelvalle <rodrigodelvalle22@hotmail.com>
2023-08-18 01:01:35 -0300

    Ultima actualización

--- js/login.js
+++ js/login.js
@@ -7,9 +7,7 @@ window.onload = function() {
         let password = document.getElementById("password").value;
         let rememberMe = document.getElementById("remember").checked;
 
-        if (username === "" || password === "") {
-            alert("Por favor, completa todos los campos.");
-        } else if (username.length > 30 || password.length > 30) {
+        if (username.length > 30 || password.length > 30) {
             alert("La contraseña no puede tener más de 30 caracteres.");
         } else {
             if (rememberMe) {
@@ -17,31 +15,11 @@ window.onload = function() {
             } else {
                 sessionStorage.setItem("username", username);
             }
-            window.location.href = "index.html";
+            Swal.fire({
+                title:'Bienvenid@ a E-mercado: ' + username,
+                icon: 'success'
+              }).then(() => { window.location.href = "index.html";
+            })
         }
     });
-
-    let bienvenida = document.getElementById("username").value = localStorage.getItem("username");
 };
-
-click = document.getElementById("login");
-
-click.addEventListener("click", function () {
-  alert('Bienvenid@ a E-mercado: ' + username.value)
-})
-
-//const checkbox = document.getElementById ("remember");
-//const username = document.getElementById ("username");
-//const btn = document.getElementById ("login");
-
-
-function guardarInfo () {
-    if (rememberMe.checked && click) {
-        sessionStorage.setItem ("username", username.value)
-    }
-    else {
-        window.location.href = "login.html"
-    }
-}
-
-checkbox.addEventListener ('change', guardarInfo)
