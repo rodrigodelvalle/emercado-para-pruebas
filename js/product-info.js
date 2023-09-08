f1a255a3a0aa9bd2147772c0a993d59c28410549
Erika Ferreira B <eri.ferreira188@gmail.com>
2023-09-08 01:33:23 -0300

    comentarios :V

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     let idProduct = localStorage.getItem("IdProduct");
     let URL_ID_PRODUCTS = "https://japceibal.github.io/emercado-api/products/" + idProduct + ".json";
+    const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/"  + idProduct + ".json";
+    const iconEstrella = '<i class="fas fa-star" style="color: #ffc800;"></i>';
+    const icoNoEstrella = '<i class="far fa-star" style="color: #ffc800;"></i>';
+    const total = 5;
+    
     localStorage.removeItem("IdProduct");
+
 
     function showProduct(product) {
         let htmlContentToAppend = "";
@@ -35,7 +41,45 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("containerItems").innerHTML = htmlContentToAppend;
     };
 
+    function showComments(itemsArray) {
+        this.productsArray = itemsArray;
+        let htmlContentToAppend = "";
+        for (let i = 0; i < itemsArray.length; i++) {
+            let estrellas = "";
+            for (let j = 1; j <= total; j++) {
+              if (j <= itemsArray[i].score) {
+                estrellas += iconEstrella;
+              } else 
+                estrellas += icoNoEstrella;
+              }
+            
+            htmlContentToAppend += `
+            <div>
+                <div>
+                <div id="coment">
+                    <div class="d-flex w-100 justify-content-between">
+                    <p class="mb-1">${itemsArray[i].user}&#160 &#160${estrellas}</p>
+                        <p class="mb-1">${itemsArray[i].dateTime} </p>
+                    </div>
+                    <br>
+                    <p class="mb-1">${itemsArray[i].description}</p>
+                </div> 
+            </div>    
+            </div>
+           
+        `
+        }
 
+        document.getElementById("containerItemsInfo").innerHTML = htmlContentToAppend;
+    }
+
+    fetch(PRODUCT_INFO_COMMENTS_URL)
+    .then(res => res.json())
+    .then(data => {
+        showComments(data);
+    });
+
+    
     fetch(URL_ID_PRODUCTS)
         .then(res => res.json())
         .then(data => showProduct(data));
