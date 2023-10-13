c5e0492c7d3d781ef7a828c35958da299a498c23
Analía Felix <110151650+analiag34@users.noreply.github.com>
2023-10-13 17:36:18 -0300

    Punto 3 - Analia

--- js/cart.js
+++ js/cart.js
@@ -24,57 +24,61 @@ document.addEventListener("DOMContentLoaded", function () {
   const URL_info = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
   fetch(URL_info)
     .then(response => response.json())
-    .then(data => showCartInfo(data.articles[0]))
-    mostrarLista();
-})
+    .then(data => {
+      showCartInfo(data.articles[0]);
+      mostrarLista();
+      let cantidades = document.getElementsByClassName('cant');
+      for (let i = 0; i < cantidades.length; i++) { //Recorre los elementos que tienen la clase "cant"
+        cantidades[i].addEventListener('input', recalcular);       // Asignar el evento 'input' a todos los elementos con la clase 'cant' después de cargar el contenido
 
+      }
+    });
+});
 
 function showCartInfo(data) {
-  let htmlContentToAppend = "";
-  htmlContentToAppend += `
-            <tr>
-              <td scope="row"><img  class="img-fluid" src="${data.image}"></img> </td>
-              <td>${data.name} </td>
-              <td> ${data.currency} ${data.unitCost}</td>
-              <td class="col"><input id="inputCart" type="number" min="1"  class="form-control w-50 mx-auto" value="${data.count}"></td>
-              <td id="subtotal"> <b> ${data.currency} ${data.unitCost}</b></td>
-            </tr>
-              `
-  document.getElementById("productosCart").innerHTML += htmlContentToAppend;
-
-  var inputCart = document.getElementById('inputCart'); // Toma los valores de cantidad de articulos y el precio por unidad
-  var precioTotal = parseFloat(data.unitCost);
-
-  function mostrar() {
-    var cantidad = parseFloat(inputCart.value); // Obtiene la cantidad de artículos
-    var subtotal = precioTotal * cantidad; // Calcula el subtotal multiplicando el precio total por la cantidad
-document.getElementById('subtotal').innerHTML = "<b>" + data.currency + " " + subtotal + "</b>"; // Modifica el subtotal con el símbolo de la moneda
-    // Cada vez que cambia el input de cantidad se ejecuta la función que modifica el subtotal.
-  }
-  inputCart.addEventListener('input', mostrar);
+  let htmlContentToAppend = `
+    <tr>
+      <td scope="row"><img class="img-fluid" src="${data.image}"></td>
+      <td>${data.name}</td>
+      <td class="precio">${data.currency} ${data.unitCost}</td>
+      <td class="col"><input id="inputCart" type="number" min="1" class="cant form-control w-50 mx-auto" value="${data.count}"></td>
+      <td class="res"><b>${data.currency} ${data.unitCost}</b></td>
+    </tr>
+  `;
+  document.getElementById("productosCart").innerHTML = htmlContentToAppend;
 }
 
-// Función que muestra los productos agregados al carrito
 function mostrarLista() {
-  // Mostrar el array almacenado en el localStorage
+  // Obtener productos del localStorage
   let arrayProductos = JSON.parse(localStorage.getItem('arrayProductos'));
-  let content = "";
-
-  for (let i = 0; i < arrayProductos.length; i++) {
-    content += `
-      <tr>
-        <td scope="row"><img class="img-fluid" src="${arrayProductos[i].images[0]}"></img></td>
-        <td>${arrayProductos[i].name}</td>
-        <td>${arrayProductos[i].currency} ${arrayProductos[i].cost}</td>
-        <td class="col"><input id="inputCart" type="number" min="1" class="form-control w-50 mx-auto" value="1"></td>
-        <td><b>${arrayProductos[i].currency} ${arrayProductos[i].cost}</b></td>
-      </tr>
-    `;
+  if (arrayProductos) {
+    let content = "";
+    for (let i = 0; i < arrayProductos.length; i++) {
+      content += `
+        <tr>
+          <td scope="row"><img class="img-fluid" src="${arrayProductos[i].images[0]}"></td>
+          <td>${arrayProductos[i].name}</td>
+          <td class="precio">${arrayProductos[i].currency} ${arrayProductos[i].cost}</td>
+          <td class="col"><input id="inputCart" type="number" min="1" class="cant form-control w-50 mx-auto" value="1"></td>
+          <td class="res"><b>${arrayProductos[i].currency} ${arrayProductos[i].cost}</b></td>
+        </tr>
+      `;
+    }
+    document.getElementById("productosCart").innerHTML += content;
   }
-
-  // Reemplazar el contenido del elemento con el id "productosCart"
-  document.getElementById("productosCart").innerHTML += content;
 }
 
-    
-    
+function recalcular() {
+  let cantidades = document.getElementsByClassName('cant'); 
+  let precios = document.getElementsByClassName('precio');
+  let preciosTotales = document.getElementsByClassName('res');
+
+  for (let i = 0; i < cantidades.length; i++) {
+    let cantidad = parseInt(cantidades[i].value);
+    let precio = parseFloat(precios[i].textContent.replace(/\D/g, '')); //busca todos los caracteres que no son dígitos ni puntos.
+    let currencySymbol = precios[i].textContent.replace(/[^A-Z]/g, ''); // Obtener símbolo de la currency busca y reemplaza cualquier carácter que no sea una letra mayúscula por una cadena vacía. Esto extrae el símbolo de la moneda.
+    let precioTotal = cantidad * precio;
+    precioTotal = Math.floor(precioTotal); // Este metodo redondea el nro para que no aparezcan los decimales.
+     preciosTotales[i].innerHTML = "<b>" + currencySymbol + " " + precioTotal + "</b>";
+  }
+}
