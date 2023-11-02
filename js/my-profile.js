006ce0400cd962de89be9d927a121dbaaf415444
Analía Felix <110151650+analiag34@users.noreply.github.com>
2023-11-01 23:56:44 -0300

    Desafiate Analia

--- js/my-profile.js
+++ js/my-profile.js
@@ -17,3 +17,37 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 })
 
+const inputFile = document.getElementById('inputFile');
+const imagenContainer = document.getElementById('imagenContainer');
+const imagenPredeterminada = document.getElementById('imagenPredeterminada');
+
+inputFile.addEventListener('change', (event) => {
+  const files = event.target.files;
+  if (files && files.length > 0) {
+    const file = files[0];
+    if (/\.(jpg|jpeg|png)$/i.test(file.name)) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        // Crear una nueva imagen
+        const img = new Image();
+        img.src = e.target.result;
+        img.style.width = '100%';
+        img.style.height = 'auto';
+
+        // Eliminar la imagen anterior del contenedor
+        while (imagenContainer.firstChild) {
+          imagenContainer.removeChild(imagenContainer.firstChild);
+        }
+
+        // Mostrar la nueva imagen en el contenedor
+        imagenContainer.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert('Formato de archivo no válido. Por favor, selecciona un archivo JPG o PNG.');
+    }
+  } else {
+    // Mostrar la imagen predeterminada si no se selecciona ninguna foto
+    imagenContainer.appendChild(imagenPredeterminada);
+  }
+});
