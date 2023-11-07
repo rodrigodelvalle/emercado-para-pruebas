030f23a7a3115b2dbf36f21916a490247eff2bab
niconilson <niconilson@hotmail.com>
2023-11-06 21:33:50 -0300

    Actualizaciones 6/11

--- js/my-profile.js
+++ js/my-profile.js
@@ -17,50 +17,96 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 })
 
+//Traigo las id de los input para guardarlos despues
+
+let primerNombre = document.getElementById("primerNombre");
+let segundoNombre= document.getElementById("segundoNombre");
+let primerApellido= document.getElementById("primerApellido");
+let segundoApellido= document.getElementById("segundoApellido");
+let email = document.getElementById("email");
+let telefono= document.getElementById("telefono");
+
+document.addEventListener('DOMContentLoaded', function(){
+let datosUsuario = JSON.parse(localStorage.getItem("guardarDatos"));
+primerNombre.value= datosUsuario.nombre1;
+segundoNombre.value= datosUsuario.nombre2;
+primerApellido.value= datosUsuario.apellido1;
+segundoApellido.value= datosUsuario.apellido2;
+email.value= datosUsuario.email;
+telefono.value=datosUsuario.telefono;
+})
+
 if(localStorage.getItem("username") || sessionStorage.getItem("username")) {
     var emailInput = document.getElementById("email");
     emailInput.value = localStorage.getItem("username") || sessionStorage.getItem("username");
 }
-
-var guardarBtn = document.getElementById("saveUserData");
+let form= document.getElementById("perfilUsuario");
 
   // Agregar evento de clic al botón de guardar
-  guardarBtn.addEventListener("click", function() {
-    // Obtener valores de los campos de entrada
-    var nombreUsuario = document.getElementById("userFirstName").value;
-    var apellidoUsuario = document.getElementById("UserFirstSurname").value;
+  form.addEventListener("submit", function() {
+    // Obtener valores
 
-    // Validar campos obligatorios
-    if (nombreUsuario && apellidoUsuario) {
-      // Guardar datos en el localStorage
-      localStorage.setItem("primernombreusuario", nombreUsuario);
-      localStorage.setItem("primerapellidousuario", apellidoUsuario);
-      // Guarda en local pero al enviar el form me vuelve a cero el input
-      var nameInput = document.getElementById("userFirstName");
-      nameInput.value = localStorage.getItem("primernombreusuario")
-      var surnameInput = document.getElementById("UserFirstSurname");
-      surnameInput.value = localStorage.getItem("primerapellidousuario")      
-    } 
+    let guardarVariables={
+nombre1:primerNombre.value, nombre2:segundoNombre.value, 
+apellido1:primerApellido.value, apellido2:segundoApellido.value,
+email:email.value, telefono:telefono.value
+
+    };
+
+localStorage.setItem("guardarDatos", JSON.stringify(guardarVariables));
   });
 
 // Funcion para validar datos de Perfil de usuario
 function validate () {
-    'use strict'
-  
-    const forms = document.querySelectorAll('.needs-validation')
-  
+  'use strict'  
+    const forms = document.querySelectorAll('.needs-validation')  
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
+          event.preventDefault();
+          event.stopPropagation();
+        }  
+        form.classList.add('was-validated');
+      }, false);
     })
-  }
+  }  
 
   document.addEventListener('DOMContentLoaded', function() {
     validate();
-  }, false);
+  }, false)
+
+  
+const inputFile = document.getElementById('inputFile');
+const imagenContainer = document.getElementById('imagenContainer');
+const imagenPredeterminada = document.getElementById('imagenPredeterminada');
+
+inputFile.addEventListener('change', (event) => {
+  const files = event.target.files;
+  if (files && files.length > 0) {
+    const file = files[0];
+    if (/\.(jpg|jpeg|png)$/i.test(file.name)) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        // Crear una nueva imagen
+        const img = new Image();
+        img.src = e.target.result;
+        img.style.width = '100%';
+        img.style.height = 'auto';
+
+        // Eliminar la imagen anterior del contenedor
+        while (imagenContainer.firstChild) {
+          imagenContainer.removeChild(imagenContainer.firstChild);
+        }
+
+        // Mostrar la nueva imagen en el contenedor
+        imagenContainer.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert('Formato de archivo no válido. Por favor, selecciona un archivo JPG o PNG.');
+    }
+  } else {
+    // Mostrar la imagen predeterminada si no se selecciona ninguna foto
+    imagenContainer.appendChild(imagenPredeterminada);
+  }
+});
